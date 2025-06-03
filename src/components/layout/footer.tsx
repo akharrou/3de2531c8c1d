262a0232f4ad896ec91088382407a1f3cf478069
Aymen Kharroubi <acad.aymenkharroubi@gmail.com>
2025-06-03 17:22:56 +0000

    in the footer, change the bird logo, to an X logo instead of a bird logo

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Linkedin, Twitter, Facebook, Heart, Youtube } from 'lucide-react';
+import { Linkedin, X, Facebook, Heart, Youtube } from 'lucide-react';
 import NavLink from '@/components/nav-link';
 
 const navItems = [
@@ -11,7 +11,7 @@ const navItems = [
 
 const socialLinks = [
   { icon: <Youtube className="h-5 w-5" />, href: 'https://youtube.com', label: 'YouTube' },
-  { icon: <Twitter className="h-5 w-5" />, href: 'https://twitter.com', label: 'Twitter' },
+  { icon: <X className="h-5 w-5" />, href: 'https://twitter.com', label: 'X' },
   { icon: <Linkedin className="h-5 w-5" />, href: 'https://linkedin.com', label: 'LinkedIn' },
   { icon: <Facebook className="h-5 w-5" />, href: 'https://facebook.com', label: 'Facebook' },
 ];
